c1085894a0aedd26c88e7a74def920c80a513071
laurenkhoury <86163632+laurenkhoury@users.noreply.github.com>
2025-06-27 12:34:44 -0500

    new jobs for chtc

--- _code/jobs_helper.R
+++ _code/jobs_helper.R
@@ -3,8 +3,8 @@
 
 suppressMessages(library(tidyverse))
 
-n_obs_lvls <- c(100, 150, 200, 300, 400)
-# n_covs_lvls <- c(4, 8, 12, 16)
+n_obs_lvls <- c(50, 100, 150, 200, 300, 400)
+n_covs_lvls <- c(4, 8, 12, 16, 20)
 p_good_covs_lvls <- c(0.25, 0.5, 0.75)
 r_ycov_lvls <- c(0.3, 0.5)    # correlation between y & good covs
 # r_cov_lvls <- c(0.3, 0.5)   # correlation among good covs
@@ -12,8 +12,8 @@ b_x_lvls <- c(0, 0.3, 0.5)    # effect of x on y
 
 jobs <- expand_grid(n_sims = 500,
                     n_obs = n_obs_lvls,
-                    b_x = b_x_lvls,
-                    n_covs = 20,
+                    b_x = 0.5,
+                    n_covs = n_covs_lvls,
                     r_ycov = r_ycov_lvls,
                     p_good_covs = p_good_covs_lvls,
                     r_cov = 0.3) |> 
@@ -22,6 +22,6 @@ jobs <- expand_grid(n_sims = 500,
   relocate(job_num)
 
 
-jobs |> write_csv("chtc/batch_20_covs/jobs.csv", col_names = FALSE)
+jobs |> write_csv("_chtc/new_20K_05/jobs.csv", col_names = FALSE)
 
 jobs |> select(-job_num) |> distinct() |> nrow()
