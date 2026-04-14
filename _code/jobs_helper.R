a71a9fab1f394cca54922635be9cdfc09e3db603
laurenkhoury <86163632+laurenkhoury@users.noreply.github.com>
2026-04-14 11:40:32 -0500

    new batches for chtc draft

--- _code/jobs_helper.R
+++ _code/jobs_helper.R
@@ -7,11 +7,11 @@ n_obs_lvls <- c(50, 100, 150, 200, 300, 400)
 n_covs_lvls <- c(4, 8, 12, 16, 20)
 p_good_covs_lvls <- c(0.25, 0.5, 0.75)
 r_ycov_lvls <- c(0.3, 0.5)    # correlation between y & good covs
-b_x_lvls <- c(0.2)            # effect of x on y
+# b_x_lvls <- c(0, 0.2, 0.5)    # effect of x on y
 
 jobs <- expand_grid(n_sims = 500,
                     n_obs = n_obs_lvls,
-                    b_x = b_x_lvls,
+                    b_x = 0.5,
                     n_covs = n_covs_lvls,
                     r_ycov = r_ycov_lvls,
                     p_good_covs = p_good_covs_lvls,
