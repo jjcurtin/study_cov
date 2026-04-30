10fa3e6eeab855500acd8dfa64c5289a4497317e
John Curtin <jjcurtin@wisc.edu>
2026-04-30 09:57:07 -0500

    reorg part 1

--- _code/mak_jobs.R
+++ _code/mak_jobs.R
@@ -1,22 +1,24 @@
-
 # R script to write jobs csv for CHTC
 
-suppressMessages(library(tidyverse))
+library(tidyverse)
 
 n_obs_lvls <- c(50, 100, 150, 200, 300, 400)
 n_covs_lvls <- c(4, 8, 12, 16, 20)
 p_good_covs_lvls <- c(0.25, 0.5, 0.75)
 r_ycov_lvls <- c(0.3, 0.5)    # correlation between y & good covs
-# b_x_lvls <- c(0, 0.2, 0.5)    # effect of x on y
+b_x_lvls <- c(0, 0.2, 0.5)    # effect of x on y
 
+# we want 40,000 simulations
+# batching jobs at 500 sims per job
+# 500 * 80 repeats = 40,0000 sims
 jobs <- expand_grid(n_sims = 500,
                     n_obs = n_obs_lvls,
-                    b_x = 0.5,
+                    b_x = b_x_lvls,
                     n_covs = n_covs_lvls,
                     r_ycov = r_ycov_lvls,
                     p_good_covs = p_good_covs_lvls,
                     r_cov = 0.3) |> 
-  slice(rep(1:n(), each = 40)) |> 
+  slice(rep(1:n(), each = 80)) |> 
   mutate(job_num = row_number()) |> 
   relocate(job_num)
 
